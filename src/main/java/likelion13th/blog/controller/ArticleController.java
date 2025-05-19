cfc6346db9234cbeecc341faaa3a98098d6580b6
Kimseounggue <seonggue12@gmail.com>
2025-05-19 20:34:37 +0900

    Refactor: 게시글 생성 및 전체 조회 로직 리팩토링

--- src/main/java/likelion13th/blog/controller/ArticleController.java
+++ src/main/java/likelion13th/blog/controller/ArticleController.java
@@ -1,6 +1,7 @@
 package likelion13th.blog.controller;
 
 import likelion13th.blog.domain.Article;
+import likelion13th.blog.service.ArticleService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,21 +12,18 @@ import java.util.List;
 @RestController
 @RequestMapping("/articles")
 public class ArticleController {
-    private final List<Article> articleDB = new ArrayList<>();
-    private Long nextId = 1L;
+
+    private final ArticleService articleService;
+
+    public ArticleController(ArticleService articleService) {
+        this.articleService = articleService;
+    }
 
     @PostMapping()
     public ResponseEntity<Article> createArticle(@RequestBody Article article){
 
-        Article newArticle = new Article(
-                article.getContent(),
-                nextId++,
-                article.getTitle(),
-                article.getAuthor(),
-                article.getPassword()
-        );
+        Article newArticle = articleService.addArticle(article);
 
-        articleDB.add(newArticle);
 
         return  ResponseEntity
                 .status(HttpStatus.CREATED)
@@ -34,8 +32,20 @@ public class ArticleController {
 
     @GetMapping()
     public ResponseEntity<List<Article>> getArticle(){
+
+        List<Article> articles = articleService.findAll();
         return ResponseEntity
                 .status(HttpStatus.OK)
-                .body(articleDB);
+                .body(articles);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Article> getArticle(@PathVariable Long id){
+        Article article = articleService.findById(id);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(article);
+
     }
 }
--- src/main/java/likelion13th/blog/service/ArticleService.java
+++ src/main/java/likelion13th/blog/service/ArticleService.java
@@ -1,4 +1,55 @@
 package likelion13th.blog.service;
 
+import likelion13th.blog.domain.Article;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@Service
 public class ArticleService {
+    private final List<Article> articleDB = new ArrayList<>();
+    private Long nextId = 1L;
+
+    public Article addArticle(Article article){
+
+
+
+        if(article.getAuthor() == null
+            || article.getContent() == null
+            || article.getTitle() == null
+            || article.getPassword() == null ) {
+            throw new IllegalArgumentException("제목,내용,작성자,비밀번호는 필수 입력 항목입니다.");
+        }
+        Article newArticle = new Article(
+                article.getContent(),
+                nextId++,
+                article.getTitle(),
+                article.getAuthor(),
+                article.getPassword()
+        );
+
+        articleDB.add(newArticle);
+
+        return newArticle;
+    }
+
+
+    public List<Article> findAll(){
+
+        //임시 DB 반환
+        return articleDB;
+    }
+
+    public Article findById(Long id){
+
+        for(Article article : articleDB){
+            if(article.getId().equals(id)){
+                return article;
+            }
+        }
+
+        throw new NoSuchElementException("해당 ID의 게시글을 찾을 수 없습니다.");
+    }
 }
