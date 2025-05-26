d457a7a56f0e9648803ae4417dcf9465ff86ea17
Kimseounggue <seonggue12@gmail.com>
2025-05-26 19:58:10 +0900

    Feat: 게시글 수정, 삭제 기능 구현

--- src/main/java/likelion_13th/blog/controller/ArticleController.java
+++ src/main/java/likelion_13th/blog/controller/ArticleController.java
@@ -1,11 +1,9 @@
 package likelion_13th.blog.controller;
 
-import likelion_13th.blog.dto.AddArticleRequest;
-import likelion_13th.blog.dto.ApiResponse;
-import likelion_13th.blog.dto.ArticleResponse;
-import likelion_13th.blog.dto.SimpleArticleResponse;
+import likelion_13th.blog.dto.*;
 import likelion_13th.blog.service.ArticleService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,8 +19,9 @@ public class ArticleController {
     @PostMapping
     public ResponseEntity<ApiResponse> createArticle(@RequestBody AddArticleRequest request){
         ArticleResponse response=articleService.addArticle(request);
-        return ResponseEntity.ok(new ApiResponse(true,201,"게시글 등록 성공",response));
-
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(new ApiResponse(true,201,"게시글 등록 성공",response));
     }
 
     /*게시글 전체 조회*/
@@ -34,6 +33,7 @@ public class ArticleController {
 
     }
 
+
     /*게시글 단일 조회*/
     @GetMapping("/{id}")
     public ResponseEntity<ApiResponse> ReadArticle(@PathVariable long id){
@@ -41,6 +41,24 @@ public class ArticleController {
         return ResponseEntity.ok(new ApiResponse(true,200,"게시글 조회 성공", response));
 
     }
-//s
 
+
+
+    /*게시글 수정*/
+    @PutMapping("/{id}")
+    public ResponseEntity<ApiResponse> updateArticle(@PathVariable long id,
+                                                     @RequestBody UpdateArticleRequest request){
+        articleService.updateArticle(id,request);
+        return ResponseEntity.ok(new ApiResponse(true,204,"게시글 수정 성공"));
+
+    }
+
+    /*게시글 삭제*/
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ApiResponse> deleteArticle(@PathVariable long id,
+                                                     @RequestBody DeleteRequest request){
+        articleService.deleteArticle(id,request);
+        return ResponseEntity.ok(new ApiResponse(true,204,"게시글 삭제 성공"));
+
+    }
 }
--- src/main/java/likelion_13th/blog/repository/ArticleRepository.java
+++ src/main/java/likelion_13th/blog/repository/ArticleRepository.java
@@ -2,10 +2,10 @@ package likelion_13th.blog.repository;
 
 import likelion_13th.blog.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public interface ArticleRepository extends JpaRepository<Article,Long> {
-// JpaRepository 클래스 상속받음
-// 엔티티 Article과 기본키 타입을 인수로
+import java.util.List;
+import java.util.Optional;
+
+public interface ArticleRepository extends JpaRepository <Article, Long>{
+
 }
